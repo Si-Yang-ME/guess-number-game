757c57080745b69d7a0cf78fae9ae047d8cd8284
许思阳 <meforwork123@outlook.com>
2026-08-10 17:13:36 +0800

    更新 guess-number-game.c

--- guess-number-game.c
+++ guess-number-game.c
@@ -4,8 +4,16 @@
 本程序的分发是希望它会有用，但没有任何担保；甚至没有对适销性或特定用途适用性的暗示担保。有关更多详细信息，请参见 GNU Affero 通用公共许可证。
 
 您应该已经随本程序收到了一份 GNU Affero 通用公共许可证的副本。如果没有，请参见 <https://www.gnu.org/licenses/>。
-你可以在https://github.com/Si-Yang-ME/guess-number-game
+你可以在<https://github.com/Si-Yang-ME/guess-number-game>
 或者其最新链接查看其源代码
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+You may view the source code at <https://github.com/Si-Yang-ME/guess-number-game> or its latest available link.
+
+
 */
 
 
